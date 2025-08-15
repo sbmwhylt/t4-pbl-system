ace1f750e473076f17595d1ec27264a5a613cef3
Shemrei Marabillo <shemrei@whyleavetown.com>
2025-08-15 14:47:02 +0800

    match info edit

--- src/components/matchpanel/MatchInfo.jsx
+++ src/components/matchpanel/MatchInfo.jsx
@@ -26,7 +26,7 @@ export default function MatchInfo({ team, matchId }) {
     <div className="flex items-center justify-between">
       {/* Team Info */}
       <div className="flex items-center space-x-4">
-        {team?.team_logo ? <img src={team.team_logo} alt="Team Logo" /> : null}
+        {team?.logo_url ? <img src={team.logo_url} alt="Team Logo" className="w-13" /> : null}
         <div>
           <h1 className="text-3xl font-bold">{team?.name || "Loading..."}</h1>
           <p className="text-gray-500">Match ID: {matchId}</p>
