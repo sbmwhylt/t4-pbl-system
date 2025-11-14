71f7cc76fec438189626c6cc4fe6031e49f46e82
Shemrei Marabillo <shemrei@whyleavetown.com>
2025-11-14 08:35:08 +0800

    added chunk size

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // in KB, e.g., 1000 KB = 1 MB
+  },
+});
